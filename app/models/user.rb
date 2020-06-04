8158c1db60e6507ad1fe128af0e38e461ec14c62
nobuyori-gh <vcd02nm120jtik@gmail.com>
2020-06-04 23:34:27 +0900

    応用課題3 いいね、コメント機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,15 @@ class User < ApplicationRecord
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
 
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
+  has_many :favorites, dependent: :destroy
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 
+  def already_favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+  end
+  # selfにはcurrent_userが入る
 end
