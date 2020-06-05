61d61329ff5a9e96185279673939c1c61fcd0ccc
nobuyori-gh <vcd02nm120jtik@gmail.com>
2020-06-05 23:48:22 +0900

    課題4 Start

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,50 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,:validatable
+  :recoverable, :rememberable, :trackable,:validatable
 
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
 
   has_many :books, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
+
+  # コメント機能
+  has_many :book_comments, dependent: :destroy
+
+  # いいね機能
   has_many :favorites, dependent: :destroy
+
+  # フォロー機能
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  # フォロワー機能
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 
+  # いいね機能
   def already_favorited?(book)
     self.favorites.exists?(book_id: book.id)
+    # selfにはcurrent_userが入る
   end
-  # selfにはcurrent_userが入る
+
+  # フォロー機能
+  # 自分自身ではないか？
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+  # フォーローを外す機能
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
 end
